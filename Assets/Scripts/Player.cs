dff1bf00e9c3c5b5cca08fc3d8e535e8eb645af2
Luciano Soares <lpsoares@gmail.com>
2020-06-05 11:22:17 -0300

    Sistema para mouse

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,21 +12,37 @@ public class Player : MonoBehaviour
     public float wait = 0.3f;
     private float timer = 0;
 
+    private Vector3 position;
+
+    void Start(){
+        position = transform.position;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float dh = Input.GetAxis("Horizontal");
-        if (( gameObject.transform.position.x > -bound || dh > 0) &&
-            ( gameObject.transform.position.x <  bound || dh < 0))
-        {
-            gameObject.transform.position += Vector3.right * dh * speed;
+     
+        position += Vector3.right * Input.GetAxis("Horizontal") * speed;
+
+        if(Input.GetAxis("Mouse X") != 0) {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position.x = worldPosition.x;
         }
+
+        if (position.x < -bound) {            
+            position.x = -bound;
+        }
+        else if (position.x > bound) {            
+            position.x = bound;
+        }
+
+        transform.position = position;
 
         timer += Time.deltaTime;
         if (timer > wait && Input.GetButton("Fire1"))
         {
             timer = 0;
-            Instantiate(bullet, gameObject.transform.position, Quaternion.identity);
+            Instantiate(bullet, transform.position, Quaternion.identity);
         }
 
     }
